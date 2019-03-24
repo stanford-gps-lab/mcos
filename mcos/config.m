325eb6fa94112e75dadce28a8c692e114f173b17
Andrew Neish <amneish@stanford.edu>
2019-03-24 12:14:33 -0700

    Changed config.m

--- mcos/config.m
+++ mcos/config.m
@@ -16,6 +16,8 @@ if nargin < 1 % Skip if plotData is being called
     minLengthOTARMessage = 0;   % MinLengthOTARMessage [messages] - minimum length that an OTAR message is. Nominally 0.
     tba = 2;    % TBA [s] - Time between authentications. Must be greater than the length of the signature
     simLength = 10000;   % SimLength [OTAR messages] - How many OTAR messages are generated for broadcast
+    % TODO: Make an option to input minimum number of full message digests
+    % received
     numUsers = 1;   % NumUsers - Number of users that start demodulating at each time interval. Nominally 1.
     weightingSchemeFile = 'Hameed-Split-ECDSA-1000.mat';   % WeightingSchemeFile - Can be an array for sensitivity analyses.
     omtConfigurationFile = 'ECDSA_RevA.mat';  % MessageConfiguration - File containing message configuration information.
